1f9f66afb24b1caaf47c6ac3eef6eb83f6f51362
Podsekina Tatyna <tany.podsekina.02@bk.ru>
2022-10-29 22:07:20 +0300

    Working with models feat: ad model and user model

---
+++ Models/Ad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Ad
+    {
+        public Guid Id { get; set; }
+
+        public int Number { get; set; }
+
+        public Guid User { get; set; }  
+
+        public string Text { get; set; }
+
+        public string Image { get; set; }
+
+        public int Rating { get; set; }
+
+        public DateTime CreatedBy { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+    }
+}
