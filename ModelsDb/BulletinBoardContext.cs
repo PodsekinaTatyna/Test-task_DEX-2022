e44fb8798c46d235ef1ac60e8887ab0e402aeba8
Podsekina Tatyna <tany.podsekina.02@bk.ru>
2022-10-30 10:14:21 +0300

    Working With Db feat: connected the database made the migration

---
+++ ModelsDb/BulletinBoardContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsDb
+{
+    public class BulletinBoardContext : DbContext
+    {
+        public DbSet<AdDb> Ads { get; set; }
+
+        public DbSet<UserDb> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                                    "Host=localhost;" +
+                                    "Port = 5432;" +
+                                    "Database = bulletin_board;" +
+                                    "Username = postgres;" +
+                                    "Password = tany0206"
+                                    );
+
+        }
+    }
+}
---
+++ ModelsDb/Migrations/20221030071112_intialDatabase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModelsDb.Migrations
+{
+    public partial class intialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    is_admin = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ads",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    number = table.Column<int>(type: "integer", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    text = table.Column<string>(type: "text", nullable: false),
+                    image = table.Column<string>(type: "text", nullable: false),
+                    rating = table.Column<int>(type: "integer", nullable: false),
+                    created_by = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    expiration_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ads", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_ads_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ads_user_id",
+                table: "ads",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ads");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
