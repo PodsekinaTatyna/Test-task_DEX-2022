17718ceac595a3c10394b7854453d5362e322570
Podsekina Tatyna <tany.podsekina.02@bk.ru>
2022-10-30 10:07:42 +0300

    Working With Db feat: added subtable models

---
+++ ModelsDb/UserDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsDb
+{
+    [Table("users")]
+    public class UserDb
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("is_admin")]
+        public bool IsAdmin { get; set; }
+
+        public ICollection<AdDb> Ads { get; set; }
+    }
+}
